c1467402552504fadaf290353adce0ff39dcbf86
Adam Taitano <adam.taitano@gmail.com>
2016-06-16 18:27:17 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "O"
+    return true
+  elsif board[index] == "X"
+    return true
+  elsif board[index] != "X"
+    return false
+  elsif board[index] != "O"
+    return false
+
+  end
+end
